49cbefeeb860b995f73440df14f9aa5abdce0731
Benjamin Plesant <benjamin.plesant@laposte.net>
2019-11-25 10:16:51 +0100

    Initial commit first pull start project

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vaccin</title>
     <link href="./assets/css/style.css" rel="stylesheet" />
 </head>
