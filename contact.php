21cace4efa8be4dc1ba9d740eca5df225c73cc38
Benjamin Plesant <benjamin.plesant@laposte.net>
2019-11-26 14:44:16 +0100

    Suppression fichier queryPdo.php

--- contact.php
+++ contact.php
@@ -2,31 +2,6 @@
 include 'includes/header.php';
 include 'includes/contact.inc.php';
 ?>
-<!--<section class="contact">-->
-<!--<h2>Nous contactez</h2>-->
-<!--        <form action="contact.php" method="post">-->
-<!--            <div class="w50">-->
-<!--                <label for="nom"></label>-->
-<!--                <input type="text" id="nom" name="nom" placeholder="Votre nom" required >-->
-<!--                --><?php //spanErr($errors, 'nom'); ?>
-<!--            </div>-->
-<!--            <div class="w50">-->
-<!--                <label for="email"></label>-->
-<!--                <input type="text" id="email" name="email" placeholder="Votre adresse email" required>-->
-<!--                --><?php //spanErr($errors, 'email'); ?>
-<!--            </div>-->
-<!---->
-<!--            <div class="w50">-->
-<!--                <label for="message"></label>-->
-<!--                <textarea name="message" id="message" rows="8" cols="80" placeholder="Votre message" required></textarea>-->
-<!--                --><?php //spanErr($errors, 'message'); ?>
-<!--            </div>-->
-<!---->
-<!--            <input type="submit" name="submitted" value="Envoyer">-->
-<!--        </form>-->
-<!---->
-<!--</section>-->
-<!---->
 <div class="wrapper">
     <div class="container">
         <h2>Nous contacter</h2>
