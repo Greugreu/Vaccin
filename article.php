3c33d0d8e2f4e8b0aa16f9903e055c32009cdf4a
antoningirard <antonin.girard.98@gmail.com>
2019-11-29 09:50:05 +0100

    Modif
    Article.php et styles.css

--- article.php
+++ article.php
@@ -4,12 +4,12 @@ require 'includes/header.php';
 
 <section id="art">
     <div class="wrap3">
-        <h1>Tous savoir sur mes vaccins</h1>
+        <h1 class="titreArticle">Tous savoir sur mes vaccins</h1>
         <section id="part1">
         <div class="blocA">
-            <h2>NOURRISSONS ET ENFANTS (DE LA NAISSANCE À 13 ANS)</h2>
+            <h2 class="sousPartie">NOURRISSONS ET ENFANTS (DE LA NAISSANCE À 13 ANS)</h2>
             <div class="bloc1">
-                <h3><a target="_blank" class="title" href="https://www.doctissimo.fr/html/dossiers/tuberculose/sa_8014_bcg_vaccin_tuberculose.htm">
+                <h3 class="titleBloc"><a target="_blank" class="title" href="https://www.doctissimo.fr/html/dossiers/tuberculose/sa_8014_bcg_vaccin_tuberculose.htm">
                         BCG : le vaccin contre la tuberculose</a></h3>
                 <p><a target="_blank" class="para" href="https://www.doctissimo.fr/html/dossiers/tuberculose/sa_8014_bcg_vaccin_tuberculose.htm">
                      <span class="italique">   Le vaccin BCG est destiné à lutter contre la tuberculose. Aujourd'hui, il n'est plus obligatoire pour
@@ -17,7 +17,7 @@ require 'includes/header.php';
             </div>
 
             <div class="bloc1">
-                <h3><a target="_blank" class="title" href="https://www.doctissimo.fr/html/dossiers/tetanos/sa_8025_vaccination_tetanos.htm">
+                <h3 class="titleBloc"><a target="_blank" class="title" href="https://www.doctissimo.fr/html/dossiers/tetanos/sa_8025_vaccination_tetanos.htm">
                         Le vaccin contre le tétanos</a></h3>
                 <p><a target="_blank" class="para" href="https://www.doctissimo.fr/html/dossiers/tetanos/sa_8025_vaccination_tetanos.htm">
                      <span class="italique">   Efficace et bien tolérée, la vaccination contre le tétanos est obligatoire en France.
@@ -25,7 +25,7 @@ require 'includes/header.php';
                       </span>  lire la suite</a></p>
             </div>
             <div class="bloc1">
-                <h3><a target="_blank" class="title" href="https://www.doctissimo.fr/html/dossiers/poliomyelite/sa_8029_vaccin_polio_poliomyelite.htm">
+                <h3 class="titleBloc"><a target="_blank" class="title" href="https://www.doctissimo.fr/html/dossiers/poliomyelite/sa_8029_vaccin_polio_poliomyelite.htm">
                         La vaccination contre la poliomyélite</a></h3>
                 <p><a target="_blank" class="para" href="https://www.doctissimo.fr/html/dossiers/poliomyelite/sa_8029_vaccin_polio_poliomyelite.htm">
                       <span class="italique">  Aucun cas de poliomyélite n'a été relevé en France depuis 1990 (hors un cas importé en 1995) grâce à
@@ -35,7 +35,7 @@ require 'includes/header.php';
         </div>
         <div class="blocA">
             <div class="bloc1">
-                <h3><a target="_blank" class="title" href="https://www.doctissimo.fr/html/dossiers/coqueluche/sa_7974_vaccin_coqueluche_enfants_proteges.htm">
+                <h3 class="titleBloc"><a target="_blank" class="title" href="https://www.doctissimo.fr/html/dossiers/coqueluche/sa_7974_vaccin_coqueluche_enfants_proteges.htm">
                         Vaccination des bébés contre la coqueluche</a></h3>
                 <p><a target="_blank" class="para" href="https://www.doctissimo.fr/html/dossiers/coqueluche/sa_7974_vaccin_coqueluche_enfants_proteges.htm">
                       <span class="italique">  Depuis janvier 2018, la vaccination contre la coqueluche est obligatoire chez les bébés de moins
@@ -43,14 +43,14 @@ require 'includes/header.php';
                         nourrisson... </span> lire la suite</a></p>
             </div>
             <div class="bloc1">
-                <h3><a target="_blank" class="title" href="https://www.doctissimo.fr/html/dossiers/haemophilus_influenzae/sa_8024_vaccin_haemophilus_influenzae.htm">
+                <h3 class="titleBloc"><a target="_blank" class="title" href="https://www.doctissimo.fr/html/dossiers/haemophilus_influenzae/sa_8024_vaccin_haemophilus_influenzae.htm">
                         La vaccination contre Haemophilus influenzae</a></h3>
                 <p><a target="_blank" class="para" href="https://www.doctissimo.fr/html/dossiers/haemophilus_influenzae/sa_8024_vaccin_haemophilus_influenzae.htm">
                      <span class="italique">   L'Haemophilus influenzae était, il y a quelques années encore, la principale cause de méningite de l'enfant
                         de moins de 5 ans.</span> lire la suite</a></p>
             </div>
             <div class="bloc1">
-                <h3><a target="_blank" class="title" href="https://www.doctissimo.fr/html/sante/mag_2001/mag0309/dossier/sa_3651_pneumocoques_vaccin.htm">
+                <h3 class="titleBloc"><a target="_blank" class="title" href="https://www.doctissimo.fr/html/sante/mag_2001/mag0309/dossier/sa_3651_pneumocoques_vaccin.htm">
                         Un vaccin contre les infections à pneumocoques</a></h3>
                 <p><a target="_blank" class="para" href="https://www.doctissimo.fr/html/sante/mag_2001/mag0309/dossier/sa_3651_pneumocoques_vaccin.htm">
                        <span class="italique"> Chaque année, plus d'un million d'enfants à travers le monde meurent d'une infection pneumococcique.
@@ -60,7 +60,7 @@ require 'includes/header.php';
         </div>
         <div class="blocA">
             <div class="bloc1">
-                <h3><a target="_blank" class="title" href="https://www.doctissimo.fr/html/dossiers/hepatites/sa_8005_hepatite_b_vaccination.htm">
+                <h3 class="titleBloc"><a target="_blank" class="title" href="https://www.doctissimo.fr/html/dossiers/hepatites/sa_8005_hepatite_b_vaccination.htm">
                         La vaccination contre l'Hépatite B</a></h3>
                 <p><a target="_blank" class="para" href="https://www.doctissimo.fr/html/dossiers/hepatites/sa_8005_hepatite_b_vaccination.htm">
                      <span class="italique">   L'hépatite virale B est une maladie grave, dont on peut se protéger depuis 1982 grâce à la vaccination.
@@ -68,14 +68,14 @@ require 'includes/header.php';
                         </span> lire la suite </a></p>
             </div>
             <div class="bloc1">
-                <h3><a target="_blank" class="title" href="https://www.doctissimo.fr/html/dossiers/rougeole/sa_7986_rougeole_vaccination_enfants.htm">
+                <h3 class="titleBloc"><a target="_blank" class="title" href="https://www.doctissimo.fr/html/dossiers/rougeole/sa_7986_rougeole_vaccination_enfants.htm">
                         Le vaccin contre la rougeole</a></h3>
                 <p><a target="_blank" class="para" href="https://www.doctissimo.fr/html/dossiers/rougeole/sa_7986_rougeole_vaccination_enfants.htm">
                       <span class="italique">  La vaccination contre la rougeole sauve un million d'enfants dans le monde chaque année.
                         Qui doit bénéficier du vaccin ROR ? Quand vacciner ? </span> lire la suite</a></p>
             </div>
             <div class="bloc1">
-                <h3><a target="_blank" class="title" href="https://www.doctissimo.fr/sante/news/comment-parler-du-vaccin-hpv-aux-adolescentes">
+                <h3 class="titleBloc"><a target="_blank" class="title" href="https://www.doctissimo.fr/sante/news/comment-parler-du-vaccin-hpv-aux-adolescentes">
                         Comment parler du vaccin HPV aux adolescentes ?</a></h3>
                 <p><a target="_blank" class="para" href="https://www.doctissimo.fr/sante/news/comment-parler-du-vaccin-hpv-aux-adolescentes">
                       <span class="italique">  La vaccination HPV contre le cancer du col de l'utérus est recommandée pour les jeunes filles de 14 ans et,
@@ -87,9 +87,9 @@ require 'includes/header.php';
         </section>
         <section id="part2">
         <div class="blocB">
-            <h2>ADOLESCENTS (14 À 19 ANS) & ADULTES (20 À 64 ANS)</h2>
+            <h2 class="sousPartie">ADOLESCENTS (14 À 19 ANS) & ADULTES (20 À 64 ANS)</h2>
             <div class="bloc2">
-                <h3><a target="_blank" class="title" href="https://www.doctissimo.fr/html/dossiers/fievre_jaune/sa_8042_fievre_jaune_vaccination.htm">
+                <h3 class="titleBloc"><a target="_blank" class="title" href="https://www.doctissimo.fr/html/dossiers/fievre_jaune/sa_8042_fievre_jaune_vaccination.htm">
                         Vaccination contre la fièvre jaune</a></h3>
                 <p><a target="_blank" class="para" href="https://www.doctissimo.fr/html/dossiers/fievre_jaune/sa_8042_fievre_jaune_vaccination.htm">
                       <span class="italique">  Si vous avez le projet de voyager dans les régions d'Afrique ou d'Amérique intertropicales sachez
@@ -97,14 +97,14 @@ require 'includes/header.php';
                          lire la suite </a></p>
             </div>
             <div class="bloc2">
-                <h3><a target="_blank" class="title" href="https://www.doctissimo.fr/sante/news/grippe-saisonniere-pourquoi-se-faire-vacciner">
+                <h3 class="titleBloc"><a target="_blank" class="title" href="https://www.doctissimo.fr/sante/news/grippe-saisonniere-pourquoi-se-faire-vacciner">
                     Grippe saisonnière : pourquoi se faire vacciner ?</a></h3>
                 <p><a target="_blank" class="para" href="https://www.doctissimo.fr/sante/news/grippe-saisonniere-pourquoi-se-faire-vacciner">
                    <span class="italique"> La campagne nationale de vaccination contre la grippe saisonnière commence ce jeudi 16 octobre.
                     L’occasion de faire le point sur ce virus...</span> lire la suite</a></p>
             </div>
             <div class="bloc2">
-                <h3><a target="_blank" class="title" href="https://www.doctissimo.fr/html/dossiers/hepatites/sa_8047_hepatite_a_vaccination.htm">
+                <h3 class="titleBloc"><a target="_blank" class="title" href="https://www.doctissimo.fr/html/dossiers/hepatites/sa_8047_hepatite_a_vaccination.htm">
                         Vaccination contre l'hépatite A</a></h3>
                 <p><a class="para" href="https://www.doctissimo.fr/html/dossiers/hepatites/sa_8047_hepatite_a_vaccination.htm">
                        <span class="italique"> L'hépatite A frappe chaque année plusieurs millions de personnes à travers le monde,
@@ -115,7 +115,7 @@ require 'includes/header.php';
         </div>
         <div class="blocB">
             <div class="bloc3">
-                <h3><a target="_blank" class="title" href="https://www.doctissimo.fr/sante/meningite/vaccin-contre-la-meningite-B">
+                <h3 class="titleBloc"><a target="_blank" class="title" href="https://www.doctissimo.fr/sante/meningite/vaccin-contre-la-meningite-B">
                         Vaccin contre les infections à méningocoque B</a></h3>
                 <p><a target="_blank" class="para" href="https://www.doctissimo.fr/sante/meningite/vaccin-contre-la-meningite-B">
                       <span class="italique">  En novembre 2012, l'agence européenne du médicament a approuvé la commercialisation du premier vaccin
@@ -124,7 +124,7 @@ require 'includes/header.php';
                         d'infection grave...</span>  lire la suite</a></p>
             </div>
             <div class="bloc3">
-                <h3><a target="_blank" class="title" href="https://www.doctissimo.fr/grossesse/news/varicelle-faut-il-vacciner-les-nourrissons">
+                <h3 class="titleBloc"><a target="_blank" class="title" href="https://www.doctissimo.fr/grossesse/news/varicelle-faut-il-vacciner-les-nourrissons">
                         Varicelle : faut-il vacciner les nourrissons ?</a></h3>
                 <p><a target="_blank" class="para" href="https://www.doctissimo.fr/grossesse/news/varicelle-faut-il-vacciner-les-nourrissons">
                       <span class="italique">  Aux Etats-Unis, le vaccin contre la varicelle a permis de faire chuter considérablement
@@ -136,10 +136,10 @@ require 'includes/header.php';
         </section>
         <section id="part3">
         <div class="blocC">
-             <h2>SENIORS (À PARTIR DE 65 ANS)
+             <h2 class="sousPartie">SENIORS (À PARTIR DE 65 ANS)
              </h2>
             <div class="bloc1">
-                <h3><a target="_blank" class="title" href="https://www.doctissimo.fr/sante/maladies-de-la-peau/zona/vaccination-contre-le-zona-en-questions">
+                <h3 class="titleBloc"><a target="_blank" class="title" href="https://www.doctissimo.fr/sante/maladies-de-la-peau/zona/vaccination-contre-le-zona-en-questions">
                         La vaccination contre le zona en questions</a></h3>
                 <p><a target="_blank" class="para" href="https://www.doctissimo.fr/sante/maladies-de-la-peau/zona/vaccination-contre-le-zona-en-questions">
                       <span class="italique">  Le vaccin contre le zona a été inscrit dans le calendrier vaccinal chez les adultes âgés de 65 à 74 ans
